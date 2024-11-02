5e217d7dd678381301f2cdf98d15727ec6c90fa8
Rio LF <s2410238017@fhooe.at>
2024-11-02 16:40:34 +0100

    übung 1 + übung 2 Zwischenstand

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,22 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+       
+        Out.println("Please enter number");
+        int Zahl1 = In.readInt();
+        Out.println ("Please enter a number");
+        int Zahl2 = In.readInt();
+        Out.println ("Please enter a number");
+        int Zahl3 = In.readInt();
+
+        if ((Zahl1 > Zahl2) & (Zahl1 > Zahl3)) {
+           System.out.println(Zahl1 + " ist die größte"); 
+        } else if ((Zahl2 > Zahl1) & (Zahl2 > Zahl3)) {
+            System.out.println(Zahl2);
+        }
+        else if ((Zahl3>Zahl1) & (Zahl3 > Zahl2))
+        System.out.println(Zahl3);
     }
 }
--- src/ue02_2.java
+++ src/ue02_2.java
@@ -1,7 +1,60 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
-    }
-}
+        Out.println("Jahreszahl");
+        int Jahreszahl = In.readInt();
+
+        Out.print("monat 1-12:  ");
+        int Monat = In.readInt();
+
+        switch (Monat) {
+            case 1: 
+            Out.print("Monat hat 31 Tage");
+                break;
+            case 2:
+            if (Jahreszahl % 4 == 0) {
+                if (Jahreszahl % 100 == 0) {
+                    if (Jahreszahl % 400 == 00) {
+                        Out.print("Monat hat 29 Tage");
+                    }
+                    else {
+                        Out.print("Monat hat 28 Tage");
+                    }
+                }
+                else {
+                    Out.print("Monat hat 29 Tage");
+                }
+            }
+            else {
+                Out.print("Monat hat 28 Tage");
+            }
+            break;
+
+            case 3:
+            Out.print ("Monat hat 31 Tage");
+            break;
+            
+            case 5:
+            Out.print ("Monat hat 31 Tage");
+            break;
+             case 7:
+             Out.print ("Monat hat 31 Tage");
+
+             case 8:
+             Out.print ("Monat hat 31 Tage");
+             break;
+
+             case 10:
+             Out.print ("Monat hat 31 Tage");
+             break;
+
+             case 12: 
+             Out.print ("Monat hat 31 Tage");
+                 default:
+            Out.print("Monat hat 30 Tage");
+                break;
+        }
+    } }
+
